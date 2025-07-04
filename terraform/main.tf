dd92e3f702912a54557b7c23049becf43601d942
Saurabh Mittal <saurabh76@gmail.com>
2025-07-04 16:31:21 +0100

    Added a block for TF version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.85.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
 provider "azurerm" {
   features {}
 }
